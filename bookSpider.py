0aefe718e7a3993cd735b3d92b9492b502cfca7a
oldyear <2811254287@qq.com>
2024-08-14 18:49:54 +0800

    初步修改脚本

--- bookSpider.py
+++ bookSpider.py
@@ -1,6 +1,10 @@
 import scrapy
 import json
 
+from scrapy.crawler import CrawlerRunner
+from twisted.internet import defer, reactor
+from addBookmark import process_bookmarks
+
 # 此爬虫用于获取具体的书籍信息
 class bookInfoSpider(scrapy.spiders.Spider):
     name = "bookInfoSpider"
@@ -44,7 +48,8 @@ class bookInfoSpider(scrapy.spiders.Spider):
         if len(tmp) > 0:
             for re in tmp:
                 bookContents.append(re.get().replace('\t', ' '))
-        book_data['目录'] = bookContents
+        # 处理目录为标准格式
+        book_data['目录'] = process_bookmarks(bookContents)
         print("书籍目录获取完成")
 
         # 以json格式存储
@@ -53,7 +58,53 @@ class bookInfoSpider(scrapy.spiders.Spider):
             f.write(json_data)
         print("文件写入完成")
 
-        
+# 定义全局变量以便于网址信息的传递——由于scrapy无法把结果返回到程序中
+book_urls = []
+
+# 此爬虫用于获取书籍的链接
+class bookLinkSpider(scrapy.spiders.Spider):
+    name = "bookSpider"
+    # 必要的网址前缀
+    base_url = "https://www.shukui.net"
+    search_url = "https://www.shukui.net/so/search.php?q="
+
+    # 这里重载init函数使其能传入url参数
+    def __init__(self, bookName=None, *args, **kwargs):
+        super(bookLinkSpider, self).__init__(*args, **kwargs)
+        self.start_url = self.search_url + bookName
+
+    def start_requests(self):
+        yield scrapy.Request(url=self.start_url, callback=self.parse)
+
+    def parse(self, response):
+        res = response.xpath('//div[@class="cate-info"]/h2/a/@href')
+        if len(res) > 0:
+            for re in res:
+                book_urls.append(self.base_url + re.get())
+
+# 设置爬虫的settings信息，后续可以在此补充或者封装为单独文件
+settings = {
+    # 'LOG_LEVEL' : "ERROR",
+    'REQUEST_FINGERPRINTER_IMPLEMENTATION' : "2.7",
+    'USER_AGENT': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0"
+}
+
+# 爬虫启动器
+runner = CrawlerRunner(settings=settings)
+
+# 设定爬虫入口函数，使其能按顺序执行
+@defer.inlineCallbacks
+def crawl(bookName):
+    yield runner.crawl(bookLinkSpider, bookName=bookName)
+    yield runner.crawl(bookInfoSpider, bookLink=book_urls)
+    reactor.stop()
+
+# 启动爬虫
+def runSpider(bookName):
+    crawl(bookName)
+    reactor.run()
+
+
 
         
         
