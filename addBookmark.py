08a3fb43d9b981549d735eca500eff1df41d9bfd
oldyear <2811254287@qq.com>
2024-08-15 12:30:39 +0800

    初步完成所有功能

--- addBookmark.py
+++ addBookmark.py
@@ -1,16 +1,52 @@
-import pikepdf
+import pikepdf, PyPDF2
 import json
-from PyPDF2 import PdfReader, PdfWriter
+
+def extract_bookmarks(pdf_path):
+    """用于检查书签是否存在，存在则返回书签列表
+
+    Args:
+        pdf_path (str): PDF书签路径
+
+    Returns:
+        list: 获取到的书签列表
+    """
+    # 打开PDF文件
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        bookmarks = extract_bookmarks_recursive(reader, reader.outlines)
+
+        if bookmarks:
+            print("PDF现有书签：")
+            for bookmark in bookmarks:
+                print(bookmark)
+        else:
+            print("PDF无书签")
+
+        return bookmarks
+        
+
+def extract_bookmarks_recursive(reader, outlines, parent=None):
+    bookmarks = []
+    for outline in outlines:
+        if isinstance(outline, list):
+            bookmarks.extend(extract_bookmarks_recursive(reader, outline, parent))
+        else:
+            title = parent + outline.title
+            pageNumber = reader.get_destination_page_number(outline)
+            bookmarks.append(f"{title} {pageNumber + 1}")
+    return bookmarks
 
 
+
+# 将书签添加到PDF文件中
 def add_bookmark_to_pdf(input_pdf_path, output_pdf_path, bookmarks, offset=0):
     # 打开原始PDF文件
     with pikepdf.open(input_pdf_path) as pdf:
         # 初始化PDF Writer
-        writer = PdfWriter()
+        writer = PyPDF2.PdfWriter()
 
         # 读取所有页面
-        reader = PdfReader(input_pdf_path)
+        reader = PyPDF2.PdfReader(input_pdf_path)
         for page in reader.pages:
             writer.add_page(page)
         
@@ -22,7 +58,8 @@ def add_bookmark_to_pdf(input_pdf_path, output_pdf_path, bookmarks, offset=0):
         # 将书签添加到原始PDF文件
         with open(output_pdf_path, 'wb') as output:
             writer.write(output)
-        
+
+# 这里根据书签的缩进得到层级关系并存储
 def add_bookmark(writer, bookmark, bookmark_hierarchy, offset=0):
     # 书签格式为缩进+编号+标题+页码
     parts = bookmark.rsplit(" ", 1)
@@ -47,6 +84,8 @@ def add_bookmark(writer, bookmark, bookmark_hierarchy, offset=0):
     bookmark_hierarchy[level] = new_bookmark
 
 
+# 根据特化规则来处理书签文件
+# TODO: 目前处理规则是根据书葵网特化的，后续可以增加处理规则以供选择
 def process_bookmarks(bookmarks):
     result = []
 
@@ -77,6 +116,8 @@ def process_bookmarks(bookmarks):
     
     return result
 
+
+# 测试代码
 if __name__ == '__main__':
     data = {}
     with open('C Primer Plus  第6版  中文版.json', 'r', encoding='utf-8') as file:
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import addBookmark
 
 from bookSpider import runSpider
 from addBookmark import process_bookmarks, add_bookmark_to_pdf
@@ -16,31 +17,38 @@ if __name__ == "__main__":
     # 启动脚本，给出说明
     print(startNotes)
     case = input("请输入你需要处理的情况：")
+    # 第一种情况需读取书签后改格式
+    bookPath = ''
+    bookmarks = []
+    offset = 0
+
     if case == '1':
         bookPath = input("请输入PDF文件地址：")
+        bookmarks = addBookmark.extract_bookmarks(bookPath)
+        bookmarks = addBookmark.process_bookmarks(bookmarks)
     elif case == '2':
         bookPath = input("请输入PDF文件地址：")
         offset = int(input("请输入偏移值："))
+        bookmarks = addBookmark.extract_bookmarks(bookPath)
     elif case == '3':
         bookName = input("请输入PDF的ISBN号：")
+        # 启动爬虫
+        print("crawl starting...")
+        runSpider(bookName)
+        print("crawl finished")
 
-    # 启动爬虫
-    # print("crawl starting...")
-    # runSpider(bookName)
-    # print("crawl finished")
+        print("书籍信息以json文件形式存储，如有需要自行查看")
 
-    # # offset值根据用户具体的PDF来确定，因此需用户自行手动输入
-    # with open(bookName + '.json', 'r', encoding='utf-8') as f:
-    #     data = json.load(f)
-    # data['目录'] = process_bookmarks(data['目录'])
-    # print("\n".join(data['目录'][:5]))
-    # print("由于不同的PDF书签偏移值不同，请根据爬取到的目录信息，输入对应偏移值")
-    # offset_user = int(input("请输入偏移值："))
+        # 要求用户检查书签页数对应是否正确，如果不正确，需输入偏移值
+        print("请检查json文件中书签对应页数是否正确，若不则输入偏移值（正确为0）:")
+        offset = int(input("请输入偏移值："))
+        bookPath = input("请输入PDF文件地址：")
+        with open(bookName + '.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            bookmarks = data['目录']
 
-    # # 添加书签
-    # print("开始添加书签...")
-    # add_bookmark_to_pdf(bookName + '.pdf', bookName + '_add.pdf', data['目录'], offset=offset_user)
-    # print("添加完成")
+    addBookmark.add_bookmark_to_pdf(bookPath, bookPath, bookmarks, offset)
+    print("处理完成")
 
     
 
